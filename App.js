4e3a92d143039c04ebff4d32a6707212a52a2916
Trif Robert <trif.robert15@gmail.com>
2023-10-31 00:39:44 +0200

    App.js text updated. Dummy.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>În sfâșit i-am dat de cap!</Text>
       <StatusBar style="auto" />
     </View>
   );
